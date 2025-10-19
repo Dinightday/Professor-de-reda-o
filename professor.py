0d10e6c895a56e64a4867d56cf2a21a95628a66a
Dinightday <darkrip1000@gmail.com>
2025-10-19 00:40:25 -0300

    professor.py

--- professor.py
+++ professor.py
@@ -2,10 +2,8 @@ from langchain_google_genai import GoogleGenerativeAI
 import streamlit as st
 import os
 
-try:
-  api_key = st.secrets('GOOGLE_API_KEY')
-except TypeError:
-  api_key = os.getenv('GOOGLE_API_KEY')
+
+api_key = st.secrets('GOOGLE_API_KEY')
 
 st.set_page_config(layout='wide', page_title='Redação')
 
